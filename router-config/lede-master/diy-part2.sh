077a5fa89d98657c6a01c9caafb25acacf589d44
misukus <57579742+misukus@users.noreply.github.com>
2022-10-13 22:58:10 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -43,7 +43,7 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
 
 # Add third-party software packages (The entire repository)
-git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall.git  package/lean/luci-app-passwall
 # Add third-party software packages (Specify the package)
 # svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
 # Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
